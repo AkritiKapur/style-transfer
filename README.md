71e9633dffc95099f10559ff38b1d8a4e316eac3
Akriti Kapur <akritikapur1@gmail.com>
2018-04-27 21:19:46 -0600

    added style transfer code and a Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#Neural Style Transfer
+
+Performs Nueral Style Transfer based on 
+Gatys, Leon A., Alexander S. Ecker, and Matthias Bethge. "Image style transfer using convolutional neural networks." Computer Vision and Pattern Recognition (CVPR), 2016 IEEE Conference on. IEEE, 2016.
+
+Code credits: https://github.com/Hvass-Labs/TensorFlow-Tutorials
--- image_utils.py
+++ image_utils.py
@@ -1,11 +1,81 @@
 import numpy as np
 import PIL.Image
+import matplotlib.pyplot as plt
 
 
 # Define Image Utils
-def get_numpy_image():
-    pass
+def get_image(filename, max_size=None):
+    image = PIL.Image.open(filename)
+
+    if max_size is not None:
+        # Calculate the appropriate rescale-factor for
+        # ensuring a max height and width, while keeping
+        # the proportion between them.
+        factor = max_size / np.max(image.size)
+
+        # Scale the image's height and width.
+        size = np.array(image.size) * factor
+
+        # The size is now floating-point because it was scaled.
+        # But PIL requires the size to be integers.
+        size = size.astype(int)
+
+        # Resize the image.
+        image = image.resize(size, PIL.Image.LANCZOS)
+
+    # Convert to numpy floating-point array.
+    return np.float32(image)
 
 
-def view_image():
-    pass
+def view_image(image):
+    # Ensure the pixel-values are between 0 and 255.
+    image = np.clip(image, 0.0, 255.0)
+
+    # Convert pixels to bytes.
+    image = image.astype(np.uint8)
+
+    # Convert to a PIL-image and display it.
+    display(PIL.Image.fromarray(image))
+
+
+def plot_images(content_image, style_image, mixed_image):
+    # Create figure with sub-plots.
+    fig, axes = plt.subplots(1, 3, figsize=(10, 10))
+
+    # Adjust vertical spacing.
+    fig.subplots_adjust(hspace=0.1, wspace=0.1)
+
+    # Use interpolation to smooth pixels?
+    smooth = True
+
+    # Interpolation type.
+    if smooth:
+        interpolation = 'sinc'
+    else:
+        interpolation = 'nearest'
+
+    # Plot the content-image.
+    # Note that the pixel-values are normalized to
+    # the [0.0, 1.0] range by dividing with 255.
+    ax = axes.flat[0]
+    ax.imshow(content_image / 255.0, interpolation=interpolation)
+    ax.set_xlabel("Content")
+
+    # Plot the mixed-image.
+    ax = axes.flat[1]
+    ax.imshow(mixed_image / 255.0, interpolation=interpolation)
+    ax.set_xlabel("Mixed")
+
+    # Plot the style-image
+    ax = axes.flat[2]
+    ax.imshow(style_image / 255.0, interpolation=interpolation)
+    ax.set_xlabel("Style")
+
+    # Remove ticks from all the plots.
+    for ax in axes.flat:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
