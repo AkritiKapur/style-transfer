d0912f380f320bc32c1d2b6b1642c7f308e79138
Akriti Kapur <akritikapur1@gmail.com>
2018-04-29 10:24:39 -0600

    Parameters of conv layer defined

--- cnn-utils.py
+++ cnn-utils.py
@@ -11,8 +11,19 @@ def get_bias(length):
     return tf.Variable(tf.constant(0.05, shape=[length]))
 
 
-def get_conv_layer():
-    pass
+def get_conv_layer(filter_height, filter_width, n_input_channels, n_channels, stride=1):
+    """
+    Returns a conv layer (tensor variable)
+    :param filter_height: Conv filter height
+    :param filter_width: Conv filter width
+    :param n_channels: Number of filters in the conv layer (current layer)
+    :param n_input_channels: number of channels in the input layer (previous layer)
+    :param stride:
+    :return: Conv layer (tensor variable)
+    """
+
+    shape = [filter_height, filter_width, n_input_channels, n_channels]
+    weight = get_weights(shape)
 
 
 def get_res_layer():
